1b68762bdffeb68d55777f6961b457a7865f6346
Andrii Dushko <andrew.dushko@gmx.com>
2015-10-27 20:31:46 +0200

    chore(Qt) update side toolbar widget

--- g/gui/qt/gsidetoolbar.py
+++ g/gui/qt/gsidetoolbar.py
@@ -1,26 +1,37 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QButtonGroup
 from PyQt5.QtCore import pyqtSignal
 
 
 class GSideToolbar(QWidget):
-    selectionChange = pyqtSignal(str)
+    selectionChanged = pyqtSignal(str)
 
     def __init__(self, buttons):
         super().__init__()
 
-        self.buttons = {}
         self.selectedId = None
 
         self.mainLayout = QVBoxLayout(self)
+        self.buttonGroup = QButtonGroup()
+        self.buttons = {}
 
         for button in buttons:
-            button = QPushButton(button[0])
-            self.mainLayout.addWidget(button)
+            b = QPushButton(button.name)
+            self.buttonGroup.addButton(b)
+            self.buttonGroup.setId(b, button.id)
+            self.buttons[button.id] = b
+            self.mainLayout.addWidget(b)
+
+        self.buttonGroup.buttonClicked.connect(self.buttonClicked)
 
         self.mainLayout.addStretch()
 
     def buttonState(self, state):
         pass
+
+    def buttonClicked(self, button : QPushButton):
+        buttonId = self.buttonGroup.id(button)
+        buttonName = self.buttons[buttonId].text()
+        self.selectionChanged.emit(buttonName)
 
     @staticmethod
     def createButton(text, icon):
