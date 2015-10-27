a242d61e5ec8ffa10f4c359bd37fef4f0a898e5f
Andrii Dushko <andrew.dushko@gmx.com>
2015-10-27 23:56:30 +0200

    chore(Qt) fill treeAlbums widget with albums

--- g/gui/qt/mainwindow.py
+++ g/gui/qt/mainwindow.py
@@ -1,10 +1,11 @@
 import sys
 
+from PyQt5.QtGui import QIcon
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeWidgetItem, QTreeWidget
 from g.gui.qt.gsidetoolbar import GSideToolbar
 from g.gui.qt.thumbview import ThumbView
+from g.core.tree import Tree
 
 
 class SideBarButton:
@@ -20,6 +21,10 @@ class SideBarButton:
 class MainWindow(QMainWindow):
     def __init__(self, *args, parent=None):
         super(MainWindow, self).__init__()
+
+        self.dbPhotos = args[0]
+        ''' :type DbPhotos'''
+        self.dbTags = args[2]
 
         loadUi('data/mainwindow.ui', self)
 
@@ -38,6 +43,37 @@ class MainWindow(QMainWindow):
         self.thumbView = ThumbView()
         self.splitterThumbs.addWidget(self.thumbView)
 
+        self.stackLeftActivateAlbums()
+        albumsTreeData = self.dbPhotos.getAlbumTree()
+
+        self.treeAlbums.headerItem().setText(0, 'Albums')
+        self.fillTreeWidget(self.treeAlbums, albumsTreeData)
+
+
+    def setAlbumsTree(self):
+        pass
+
+    def stackLeftActivateAlbums(self):
+        self.stackLeft.setCurrentWidget(self.treeAlbums)
+
+    def stackLeftActivateTags(self):
+        self.stackLeft.setCurrentWidget(self.treeTags)
+
+    def fillTreeWidget(self, widget : QTreeWidget, tree : Tree):
+        def helper(t : Tree, parent):
+            item = QTreeWidgetItem()
+            item.setText(0, t.name)
+            item.setIcon(0, QIcon('data/gfx/folder.png'))
+            if type(parent) == QTreeWidget:
+                parent.insertTopLevelItem(0, item)
+            else:
+                parent.addChild(item)
+            for ch in t.children:
+                helper(ch, item)
+
+        widget.setColumnCount(1)
+        helper(tree, widget)
+
     def onLeftSideToolChange(self, buttonId):
         if buttonId == 'Albums':
             print('Albums!!')
@@ -50,7 +86,7 @@ class MainWindow(QMainWindow):
     def start(*args):
         app = QApplication(sys.argv)
 
-        w = MainWindow(args)
+        w = MainWindow(*args)
         w.show()
 
         sys.exit(app.exec_())
