5366b54c02ccc25681ba934603d8f159490cfd6e
Andrii Dushko <andrew.dushko@gmx.com>
2015-10-27 20:34:07 +0200

    chore(Qt) add thumbnails view widget to main window

--- g/gui/qt/mainwindow.py
+++ g/gui/qt/mainwindow.py
@@ -4,6 +4,17 @@ from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from g.gui.qt.gsidetoolbar import GSideToolbar
+from g.gui.qt.thumbview import ThumbView
+
+
+class SideBarButton:
+    def __init__(self, name, buttonId, icon):
+        assert type(buttonId) == int;
+        assert type(name) == str
+
+        self.id = buttonId
+        self.name = name
+        self.icon = icon
 
 
 class MainWindow(QMainWindow):
@@ -12,17 +23,28 @@ class MainWindow(QMainWindow):
 
         loadUi('data/mainwindow.ui', self)
 
-        leftToolbarButtons = [['albums', 'Albums', 'image'],
-                ['tags', 'Tags', 'accessories-text-editor']]
-        rightToolbarButtons = [['properties', 'Properties', 'image'],
-                ['metadata', 'Metadata', 'image']]
+        leftToolbarButtons = [SideBarButton('Albums', 1, 'image'),
+                SideBarButton('Tags', 2, 'image')]
+        rightToolbarButtons = [SideBarButton('Properties', 1, 'image'),
+                SideBarButton('Metadata', 2, 'image')]
         self.leftSideToolbar = GSideToolbar(leftToolbarButtons)
+        self.leftSideToolbar.selectionChanged.connect(self.onLeftSideToolChange)
         self.rightSideToolbar = GSideToolbar(rightToolbarButtons)
 
         self.centralLayout.insertWidget(-1, self.rightSideToolbar)
         self.centralLayout.insertWidget(0, self.leftSideToolbar)
 
 
+        self.thumbView = ThumbView()
+        self.splitterThumbs.addWidget(self.thumbView)
+
+    def onLeftSideToolChange(self, buttonId):
+        if buttonId == 'Albums':
+            print('Albums!!')
+            items = [i for i in range(100)]
+            self.thumbView.setItems(items)
+        else:
+            print('Button clicked: ', buttonId)
 
     @staticmethod
     def start(*args):
