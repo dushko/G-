f1c0c10a497693d0311c29609e692b0faa714f56
Andrii Dushko <andrew.dushko@gmx.com>
2015-10-29 22:04:10 +0200

    refactor(Db) move thumbs database to new file

--- g/core/db/database.py
+++ g/core/db/database.py
@@ -208,31 +208,3 @@ class Thumb(object):
         pass
 
 
-class DBThumbs(object):
-    def getByHash(self, hash):
-        raise NotImplementedError()
-    def getByFilePath(self, path):
-        raise NotImplementedError()
-    def getById(self, id):
-        raise NotImplementedError()
-
-    def removeByHash(self, hash):
-        raise NotImplementedError()
-    def removeByFilePath(self, path):
-        raise NotImplementedError()
-    def removeById(self, id):
-        raise NotImplementedError()
-
-class DbThumbsFile(DBThumbs):
-    def __init__(self, path):
-        self.path = path
-
-    def getByHash(self, thumbHash):
-        subDir = thumbHash[:2]
-        fname = os.path.join(self.path, subDir, thumbHash[2:])
-        if os.path.exists(fname):
-            with open(fname, 'rb') as f:
-                data = f.readall()
-                return Thumb(0, data)
-        else:
-            return None
