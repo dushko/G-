f11c2bfe1b6a5fa945542bf08a51591a54f900f6
Andrii Dushko <andrew.dushko@gmx.com>
2015-11-02 01:19:58 +0200

    chore(Db) add sqlite implementation for thumbs database

--- g/core/db/dbthumbs.py
+++ g/core/db/dbthumbs.py
@@ -1,4 +1,6 @@
-import os
+import os, sqlite3
+
+from PIL import Image
 
 from g.core.db.database import Thumb
 
@@ -18,17 +20,56 @@ class DBThumbs(object):
     def removeById(self, id):
         raise NotImplementedError()
 
+    def add(self, path : str):
+        pass
 
-class DbThumbsFile(DBThumbs):
-    def __init__(self, path):
-        self.path = path
+class DbThumbsSqlite(DBThumbs):
+    def __init__(self, base):
+        self.base = base
+        self.cur = None
+        self.conn = None
 
-    def getByHash(self, thumbHash):
-        subDir = thumbHash[:2]
-        fname = os.path.join(self.path, subDir, thumbHash[2:])
-        if os.path.exists(fname):
-            with open(fname, 'rb') as f:
-                data = f.readall()
-                return Thumb(0, data)
+        self.__openBase()
+
+    def __openBase(self):
+        self.conn = sqlite3.connect(self.base)
+
+        self.cur = self.conn.cursor()
+        self.cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='thumbnails'")
+        if self.cur.fetchone() is None:
+            self.cur.execute("CREATE TABLE thumbnails("
+                    "path STRING,"
+                    "thumbnail BLOB,"
+                    "PRIMARY KEY(path))")
+
+    def getByFilePath(self, thumbPath):
+        self.cur.execute("SELECT thumbnail FROM thumbnails WHERE path=?", (thumbPath,))
+        thumb = self.cur.fetchone()
+        if thumb:
+            return thumb[0]
         else:
             return None
+
+    def addByFilePath(self, thumbPath, thumbnail):
+        self.cur.execute("SELECT path FROM thumbnails WHERE path=?", (thumbPath,))
+        if self.cur.fetchone():
+            self.cur.execute("UPDATE thumbnails SET thumbnail=? WHERE path=?",
+                    (thumbnail, thumbPath))
+        else:
+            self.cur.execute("INSERT INTO thumbnails(path, thumbnail) VALUES(?, ?)",
+                    (thumbPath, thumbnail))
+
+        self.conn.commit()
+
+if __name__ == '__main__':
+    db = DbThumbsSqlite('thumbs.db')
+    size = (256, 256)
+    d = '/media/data/ph/marn2'
+    paths = os.listdir('/media/data/ph/marn2')
+    paths = [os.path.join(d, p) for p in paths]
+    for p in paths:
+        print(p)
+        thumb = g.core.db.thumbreader.getThumb(p, size)
+        db.addByFilePath(p, thumb)
+
+        retr = db.getByFilePath(p)
